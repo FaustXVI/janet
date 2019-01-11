76ef11d08779ffbb1037b9df624718b1b24dcfa7
FaustXVI <xavier.detant@gmail.com>
2019-01-11 18:41:21 +0100

    Wait for led to be exported if necessary

--- janet/src/main.rs
+++ janet/src/main.rs
@@ -4,10 +4,14 @@ use janet::led::blink;
 use sysfs_gpio::Pin;
 use sysfs_gpio::Direction;
 use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let led = Pin::new(23);
     led.export().unwrap();
-    led.set_direction(Direction::Low).unwrap();
+    if led.set_direction(Direction::Low).is_err() {
+        sleep(Duration::from_millis(500));
+        led.set_direction(Direction::Low).unwrap();
+    };
     blink(&led, |d| sleep(d));
 }
