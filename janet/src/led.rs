7673e89745bbec6d43b41695655ddd1c9fae5787
FaustXVI <xavier.detant@gmail.com>
2019-01-11 18:47:52 +0100

    Refactor tests

--- janet/src/led.rs
+++ janet/src/led.rs
@@ -20,39 +20,17 @@ pub fn blink(led: &impl Switchable, mut pause: impl FnMut(Duration) -> ()) -> ()
 #[cfg(test)]
 mod should {
     use super::*;
-    use std::cell::RefCell;
-
-    pub struct FakeLed {
-        states: RefCell<Vec<bool>>
-    }
-
-    impl FakeLed {
-        pub fn new() -> Self {
-            FakeLed {
-                states: RefCell::new(Vec::new())
-            }
-        }
-    }
-
-    impl Switchable for FakeLed {
-        fn switch_on(&self) -> () {
-            self.states.borrow_mut().push(true);
-        }
-
-        fn switch_off(&self) -> () {
-            self.states.borrow_mut().push(false);
-        }
-    }
+    use crate::pin::mock::InMemoryPin;
 
     #[test]
     fn alternate_on_and_off() {
-        let led = FakeLed::new();
+        let led = InMemoryPin::new();
         let mut durations: Vec<Duration> = vec![];
         blink(&led, |d| { durations.push(d) });
         let vec = led.states.into_inner();
         let truthfull = vec.iter().filter(|&&b| b).count();
         let falsefull = vec.iter().filter(|&&b| b).count();
-        assert_eq!(truthfull, 5);
+        assert_eq!(truthfull, 10);
         assert_eq!(truthfull, falsefull);
         assert_eq!(durations.len(), truthfull + falsefull)
     }
--- janet/src/pin.rs
+++ janet/src/pin.rs
@@ -10,3 +10,31 @@ impl Switchable for Pin {
         self.set_value(0).unwrap()
     }
 }
+
+#[cfg(test)]
+pub mod mock {
+    use super::*;
+    use std::cell::RefCell;
+
+    pub struct InMemoryPin {
+        pub states: RefCell<Vec<bool>>
+    }
+
+    impl InMemoryPin {
+        pub fn new() -> Self {
+            InMemoryPin {
+                states: RefCell::new(Vec::new())
+            }
+        }
+    }
+
+    impl Switchable for InMemoryPin {
+        fn switch_on(&self) -> () {
+            self.states.borrow_mut().push(true);
+        }
+
+        fn switch_off(&self) -> () {
+            self.states.borrow_mut().push(false);
+        }
+    }
+}
