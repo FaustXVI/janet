2e44f92601b892c068d9b7203989404023f5cc45
FaustXVI <xavier.detant@gmail.com>
2019-01-14 18:57:33 +0100

    Simplify dev env

--- default.nix
+++ default.nix
@@ -1,24 +1,9 @@
 with import <nixos> {};
-let
-  myboost = pkgs.boost.override { enableShared = false; enabledStatic = true; };
-in 
 stdenv.mkDerivation {
         name = "janet";
         buildInputs = [
           rustup
           arduino
-          myboost
-          myboost.out
-          myboost.dev
-          cmake
-          lsb-release
           fritzing
         ];
-        shellHook = ''
-          export PATH="$PATH:${arduino}/share/arduino/hardware/tools/avr/bin"
-          export ARDUINO_SDK_PATH="${arduino}/share/arduino/"
-          export BOOST_ROOT="${myboost.dev}"
-          export BOOST_INCLUDEDIR="${myboost.dev}/include/boost"
-          export BOOST_LIBRARYDIR="${myboost.out}/lib"
-        '';
     }
