0ac8ee1ccd764b6fb2dc7a4ceefdb55db2a22b2b
FaustXVI <xavier.detant@gmail.com>
2019-01-14 18:53:06 +0100

    Send command given on command line

--- janet/src/main.rs
+++ janet/src/main.rs
@@ -10,21 +10,35 @@ use janet::message_sender::Message;
 use janet::message_sender::Channel;
 use janet::message_sender::SubChannel;
 use janet::message_sender::Status;
+use std::env;
 
 fn main() {
-    let pin = Pin::new(23);
-    pin.export().unwrap();
-    if pin.set_direction(Direction::Low).is_err() {
-        sleep(Duration::from_millis(500));
-        pin.set_direction(Direction::Low).unwrap();
-    };
-    let sender = MessageSender::new(Blyss::new(pin));
-    let switch_on = Message::new(0x7057, Channel::ChannelC, SubChannel::Channel1, Status::On);
-    let switch_off = Message::new(0x7057, Channel::ChannelC, SubChannel::Channel1, Status::Off);
-    for _ in 0..10 {
-        sender.send(&switch_on);
-        sleep(Duration::from_secs(5));
-        sender.send(&switch_off);
-        sleep(Duration::from_secs(5));
-    };
+    let args: Vec<String> = env::args().collect();
+    let status = from_command_line(args);
+    match status {
+        Some(status) => {
+            let pin = Pin::new(23);
+            pin.export().unwrap();
+            if pin.set_direction(Direction::Low).is_err() {
+                sleep(Duration::from_millis(500));
+                pin.set_direction(Direction::Low).unwrap();
+            };
+            let sender = MessageSender::new(Blyss::new(pin));
+            let message = Message::new(0x7057, Channel::ChannelC, SubChannel::Channel1, status);
+            sender.send(&message);
+        }
+        None => println!("usage : janet [On|Off]")
+    }
+}
+
+fn from_command_line(args: Vec<String>) -> Option<Status> {
+    if args.len() != 2 {
+        None
+    } else {
+        match args[1].as_str() {
+            "On" => Some(Status::On),
+            "Off" => Some(Status::Off),
+            _ => None
+        }
+    }
 }
