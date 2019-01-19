f54b6fb8125b461ecf51dc84110977f6cbcebb39
FaustXVI <xavier.detant@gmail.com>
2019-01-19 16:50:51 +0100

    Running janet with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,77 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 extern crate sysfs_gpio;
 
 use janet::blyss_sender::Status;
-use std::env;
 use janet::raspberry::create_house;
+use janet::raspberry::create_fake_house;
+use rocket::State;
+use janet::house::House;
+use std::sync::Mutex;
+use std::sync::Arc;
+use janet::house::MyHouse;
+use janet::blyss_sender::MessageSender;
+use janet::blyss::Blyss;
+use janet::raspberry::FakeDigitalOutput;
+use std::sync::MutexGuard;
+use rocket::Response;
+use rocket::http::ContentType;
+use rocket_contrib::templates::Template;
+use rocket_contrib::serve::StaticFiles;
+use rocket::response::Redirect;
+use rocket::request::Form;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let status = from_command_line(args);
-    match status {
-        Some(status) => {
-            let house = create_house();
-            house.light(status);
-        }
-        None => println!("usage : janet [On|Off]")
+
+#[macro_use]
+extern crate rocket;
+
+pub struct Resolver {
+    house: Arc<Mutex<House + Send>>
+}
+
+impl Resolver {
+    fn get_house(&self) -> MutexGuard<House + Send + 'static> {
+        self.house.lock().unwrap()
     }
 }
 
-fn from_command_line(arg: Vec<String>) -> Option<Status> {
-    let a = arg.get(0).unwrap();
-    parse(a.clone())
+#[derive(FromForm)]
+struct NewStatus{
+    status: String
 }
+
+#[post("/light", data = "<status>")]
+fn light(resolver: State<Resolver>, status: Form<NewStatus>) -> Redirect {
+    if let Some(status) = parse(status.into_inner().status) {
+        let house = resolver.get_house();
+        house.light(status);
+    }
+    Redirect::to("/")
+}
+
+fn main() {
+    let house = create_fake_house();
+    let resolver = Resolver {
+        house: Arc::new(Mutex::new(house))
+    };
+    rocket::ignite()
+        .attach(Template::fairing())
+        .manage(resolver)
+        .mount("/", StaticFiles::from("static"))
+        .mount("/api", routes![light]).launch();
+}
+
+//fn main() {
+//    let args: Vec<String> = env::args().collect();
+//    let status = from_command_line(args);
+//    match status {
+//        Some(status) => {
+//            let house = create_house();
+//            house.light(status);
+//        }
+//        None => println!("usage : janet [On|Off]")
+//    }
+//}
 
 fn parse(arg: String) -> Option<Status> {
     match arg.as_str() {
--- src/raspberry.rs
+++ src/raspberry.rs
@@ -6,8 +6,11 @@ use crate::blyss_sender::MessageSender;
 use crate::blyss::Blyss;
 use crate::house::MyHouse;
 use std::thread::sleep;
+use std::sync::Arc;
+use std::sync::Mutex;
+use crate::pin::DigitalOutput;
 
-pub fn create_house() -> Box<House> {
+pub fn create_house() -> impl House {
     let pin = Pin::new(23);
     pin.export().unwrap();
     if pin.set_direction(Direction::Low).is_err() {
@@ -15,5 +18,31 @@ pub fn create_house() -> Box<House> {
         pin.set_direction(Direction::Low).unwrap();
     };
     let sender = MessageSender::new(Blyss::new(pin));
-    Box::new(MyHouse::new(sender))
+    MyHouse::new(sender)
+}
+
+pub fn create_fake_house() -> impl House {
+    let pin = FakeDigitalOutput::new(23);
+    let sender = MessageSender::new(Blyss::new(pin));
+    MyHouse::new(sender)
+}
+
+pub struct FakeDigitalOutput {
+    pin: usize
+}
+
+impl FakeDigitalOutput {
+    fn new(pin: usize) -> Self {
+        FakeDigitalOutput { pin }
+    }
+}
+
+impl DigitalOutput for FakeDigitalOutput {
+    fn high_during(&self, duration: Duration) -> () {
+        println!("high for {:?} on {}", duration, self.pin)
+    }
+
+    fn low_during(&self, duration: Duration) -> () {
+        println!("low for {:?} on {}", duration, self.pin)
+    }
 }
