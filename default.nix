c2e431aa19c311f1397f751551d4ecdccb8d46dc
FaustXVI <xavier.detant@gmail.com>
2019-01-19 12:31:42 +0100

    Cross compile with no docker on nightly

--- default.nix
+++ default.nix
@@ -1,9 +1,16 @@
-with import <nixos> {};
-stdenv.mkDerivation {
-        name = "janet";
-        buildInputs = [
-          rustup
-          arduino
-          fritzing
-        ];
-    }
+let 
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  arm = import <nixpkgs> { 
+    crossSystem = pkgs.lib.systems.examples.raspberryPi; };
+in
+with pkgs {};
+pkgs.stdenv.mkDerivation {
+  name = "janet";
+  buildInputs = [
+    pkgs.arduino
+    pkgs.fritzing
+    (pkgs.rustChannelOfTargets "nightly" "2018-12-30" [ "x86_64-unknown-linux-gnu" "arm-unknown-linux-gnueabihf" ])
+    arm.stdenv.cc
+  ];
+}
